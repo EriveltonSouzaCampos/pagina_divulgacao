4b0cb8a4cefb314b1339ce80cc1dfac8d1bb115c
Erivelton Campos <erivelton.campos@injunior.com.br>
2024-01-23 19:11:24 -0300

    feat: setting variables and includes

---
+++ app/public/wp-content/themes/paginaDivulgacao/functions.php
@@ -0,0 +1,12 @@
+<?php 
+//var
+define('ROOT_DIR', get_theme_file_path());
+define('STYLES_DIR', get_template_directory_uri() . '/assets/css');
+define('IMAGES_DIR', get_template_directory_uri() . '/assets/images');
+define('SCRIPTS_DIR', get_template_directory_uri() . '/assets/js');
+define('INCLUDES_DIR', ROOT_DIR . '/includes');
+
+//Includes
+include_once(INCLUDES_DIR . '/equeue.php')
+
+?>
