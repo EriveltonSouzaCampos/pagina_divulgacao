610aead6f10fa440fa49dd5f33c3d86d092ad822
Erivelton Campos <erivelton.campos@injunior.com.br>
2024-01-23 19:02:31 -0300

    chore: creating structure of header and add favicon

--- app/public/wp-content/themes/paginaDivulgacao/header.php
+++ app/public/wp-content/themes/paginaDivulgacao/header.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>
+    <?php bloginfo('name') ?> -
+    <?php the_title(); ?>
+    </title>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
     <header>
-        
+        <div class="conter_header">
+            <div class="header">
+                <img src="" alt="Logo da empresa divulgada">
+                <a href="">About us</a>
+                <a href="">About Event</a>
+                <a href="">Contact Us</a>
+            </div>
+        </div>
     </header>
