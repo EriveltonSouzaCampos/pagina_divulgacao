3427d4b247a9c314b6d479afa67e9bc4e60b82bb
Erivelton Campos <erivelton.campos@injunior.com.br>
2024-01-24 20:21:27 -0300

    chore: add new style to enqueue

--- app/public/wp-content/themes/paginaDivulgacao/includes/enqueue.php
+++ app/public/wp-content/themes/paginaDivulgacao/includes/enqueue.php
@@ -4,12 +4,13 @@ function enqueue(){
     wp_register_style('style', get_stylesheet_uri(), [], '1.0.0', false);
     wp_register_style( 'head', STYLES_DIR . '/header_style.css', [], '1.0.0', false );
     wp_register_style( 'reset', STYLES_DIR . '/reset.css', [], '1.0.0', false );
-
+    wp_register_style( 'home', STYLES_DIR . '/main_pageHome_style.css', [], '1.0.0', false );
 
     //enqueue
     wp_enqueue_style('style');
     wp_enqueue_style( 'head' );
     wp_enqueue_style( 'reset' );
+    wp_enqueue_style('home');
     
 }
 ?>
