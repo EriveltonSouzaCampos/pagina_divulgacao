2385f5bdc703ff4f4a4321048733df254828fbbe
Erivelton Campos <erivelton.campos@injunior.com.br>
2024-01-24 20:21:03 -0300

    feat: created structure and style of section event

--- app/public/wp-content/themes/paginaDivulgacao/page-home.php
+++ app/public/wp-content/themes/paginaDivulgacao/page-home.php
@@ -10,6 +10,15 @@
             <h1>Name of event</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim quos harum modi nostrum suscipit minima. Soluta quasi minima nulla, recusandae deserunt, laboriosam quae perferendis iusto eligendi, et id odit. Nisi?</p>
         </div>
+        <div class="linear_blue"></div>
+        <div class="about_the_event_body">
+            <h1>About us</h1>
+            <div class="textof_event">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis totam, consequatur dicta reprehenderit illum dolorum. Est quod architecto explicabo eos esse distinctio vel illo ut, quo dicta quasi aperiam rem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptates incidunt minima qui ea aut amet facilis similique, ut, saepe non nulla placeat sed enim quos molestias! Voluptatum, deserunt provident?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione esse rerum illum labore dignissimos, excepturi et aperiam, sit perferendis laudantium iure totam, atque quaerat! Molestiae explicabo nobis accusantium laudantium recusandae!</p>
+            </div>
+            <a href="">More about</a>
+        </div>
    </div>
 </main>
 
