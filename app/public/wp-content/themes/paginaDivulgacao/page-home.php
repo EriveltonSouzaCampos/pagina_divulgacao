6a33c45c73bb907ffd7a242fd39e6c05ef578c62
Erivelton Campos <erivelton.campos@injunior.com.br>
2024-01-24 19:14:58 -0300

    feat: creating section on page home

--- app/public/wp-content/themes/paginaDivulgacao/page-home.php
+++ app/public/wp-content/themes/paginaDivulgacao/page-home.php
@@ -4,7 +4,13 @@
 <?php get_header(); ?>
 
 <main>
-   
+   <div class="conter_body">
+        <div class="about_client_body">
+            <div class="filter"></div>
+            <h1>Name of event</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim quos harum modi nostrum suscipit minima. Soluta quasi minima nulla, recusandae deserunt, laboriosam quae perferendis iusto eligendi, et id odit. Nisi?</p>
+        </div>
+   </div>
 </main>
 
 <?php get_footer(); ?>
